fc60fbd2b996eac9f78bdd93fa74400fe71ffb88
MaverickEdwards <r.edwards0917@gmail.com>
2022-11-17 17:33:36 -0500

    commit tree bbd05acba678a965d9606b8f31580c83671a439d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string){
+    const result = drivers.filter(driversName => drivers.Name === string)
+    return result;
+}
+function fuzzyMatch(drivers, string){
+    const result = drivers.filter(driversName => driversName.startsWith(string))
+    return result;
+}
+function matchName(drivers, string){
+    const result = drivers.filter(driversName => driversName.match)
+    return result;
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
